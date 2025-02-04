419fedc9100053bc997a5029bfd6e305e3cb14e6
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-02-04 17:45:43 +0100

    Skrev ut datan i tabell i html

--- src/js/main.js
+++ src/js/main.js
@@ -23,6 +23,8 @@ function toggleMenu() {
 
 /* Skriver ut kurserna */
 
+let courses = [];
+
 window.onload = () => {
     loadCourses();
 }
@@ -30,10 +32,31 @@ window.onload = () => {
 async function loadCourses() {
     try {
         const response = await fetch("https://webbutveckling.miun.se/files/ramschema_ht24.json");
-        const data = await response.json();
+        if (!response.ok) {
+            throw new Error("Fel vid anslutning till JSON-data...");
+        }
+        courses = await response.json();
+        printCourses (courses);
 
-        console.table(data);
     } catch(error){
         console.error(error)
+        document.querySelector("#error").innerHTML = "<p>Just nu har vi problem med anslutning - försök igen senare.</p>";
     }
+}
+
+function printCourses (data) {
+    const courseEl = document.querySelector("#courseTableBody");
+    courseEl.innerHTML = "";
+
+    data.forEach(course => {
+        const row = document.createElement("tr"); // Skapa en ny rad
+
+        row.innerHTML = `
+            <td>${course.code}</td>
+            <td>${course.coursename}</td>
+            <td>${course.progression}</td>
+        `;
+
+        courseEl.appendChild(row); // Lägg till raden i tabellen
+    });
 }
